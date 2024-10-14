c42657708c8496b126e998dd1f8decba7c5f3669
JinhyeongSeo <jh4clover@naver.com>
2024-10-14 17:12:12 +0900

    #10 InterviewFeedback.js수정중

--- src/pages/Choice.js
+++ src/pages/Choice.js
@@ -6,9 +6,10 @@ import choicefe from '../assets/choicefe.png'; // 이미지 파일 import
 import choicebe from '../assets/choicebe.png'; // 이미지 파일 import
 import styled from 'styled-components';
 import { Modal, Button as ModalButton } from 'react-bootstrap'; // Modal 컴포넌트 추가
-import Input from '../components/Input'
-import Swal from 'sweetalert2'; // SweetAlert2 import
-
+import Input from '../components/Input';
+import Swal from 'sweetalert2';
+import axios from "axios";
+import {useNavigate} from "react-router-dom"; // SweetAlert2 import
 
 // 모달 창 스타일
 const ModalContent = styled.div`
@@ -17,14 +18,15 @@ const ModalContent = styled.div`
     padding: 20px;
     font-family: PretendardL;
 `;
-  
 
 const Choice = () => {
     const { setHeaderMode } = useHeaderMode();
     const [roomTitle, setRoomTitle] = useState('');
+    const [roomOccupation, setRoomOccupation] = useState('');
     const [showAddModal, setShowAddModal] = useState(false);
+    let accessToken = window.localStorage.getItem('accessToken');
+    const navigate = useNavigate();
 
-  
     const handleShowAddModal = () => {
         setShowAddModal(true);
     };
@@ -33,26 +35,47 @@ const Choice = () => {
         setShowAddModal(false);
     };
 
-    const handleRoomCreation = () => {
-        console.log('방 생성 시도'); // 추가
-        console.log(`현재 방 제목: '${roomTitle}'`); // 현재 방 제목 로그 추가
-        if (roomTitle.trim() === '') {
+    const handlePostInterviewRoom = async () => {
+        if (roomTitle.trim() === '' || roomOccupation.trim() === '') {
             Swal.fire({
                 icon: 'warning',
-                title: '방 제목을 입력해주세요.',
+                title: '모든 필드를 입력해주세요.',
                 confirmButtonText: '확인'
             }).then(() => {
-                setRoomTitle(''); // 스왈창 확인 후 방 제목 초기화
-                setShowAddModal(false); // 모달 닫기
+                setRoomTitle(''); // 방 제목 초기화
+                setRoomOccupation(''); // 방 직군 초기화
             });
             return;
         }
-        console.log(`방 제목: ${roomTitle}`);
-        // 방 생성 로직 추가
-        setShowAddModal(false);
-        setRoomTitle('');
+
+        console.log(`방 제목: ${roomTitle}, 방 직군: ${roomOccupation}`);
+        navigate('/WebRTC');
+
+        try {
+            const response = await axios.post('https://server.nextpick.site/rooms',
+                {
+                    title: roomTitle,
+                    occupation: roomOccupation,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                }
+            );
+            // 방 생성 성공 시 추가 동작 (예: 알림)
+            Swal.fire({
+                icon: 'success',
+                title: '방이 성공적으로 생성되었습니다.',
+                confirmButtonText: '확인'
+            });
+            setShowAddModal(false); // 모달 닫기
+            navigate('/interviewRoom', { state: roomOccupation });
+        } catch (error) {
+            alert("방 만드는 중 오류가 발생했습니다. 다시 시도해주세요.");
+        }
     };
-    
 
     useEffect(() => {
         setHeaderMode('main');
@@ -67,7 +90,7 @@ const Choice = () => {
             <Button color="transparent" radius="5px" hoverColor="#FFFFFF" onClick={handleShowAddModal}>
                 <img src={choicebe} alt="be" style={{ width: '350px', height: '340px' }} />
             </Button>
-    
+
             {/* 방 만들기 모달 */}
             <Modal show={showAddModal} onHide={handleCloseAddModal}>
                 <Modal.Header closeButton>
@@ -80,11 +103,21 @@ const Choice = () => {
                             value={roomTitle}
                             onChange={(e) => setRoomTitle(e.target.value)}
                         />
+                        <select
+                            value={roomOccupation}
+                            onChange={(e) => setRoomOccupation(e.target.value)}
+                            style={{ marginTop: '10px', padding: '10px', borderRadius: '5px' }}
+                        >
+                            <option value="" disabled>방 직군 선택</option>
+                            <option value="BE">백앤드</option>
+                            <option value="FE">프론트앤드</option>
+                            {/* 필요한 경우 추가 직군 옵션을 여기에 추가하세요 */}
+                        </select>
                     </ModalContent>
                 </Modal.Body>
                 <Modal.Footer>
                     <ModalButton variant="secondary" onClick={handleCloseAddModal}>닫기</ModalButton>
-                    <ModalButton variant="primary" onClick={handleRoomCreation}>방 만들기</ModalButton>
+                    <ModalButton variant="primary" onClick={handlePostInterviewRoom}>방 만들기</ModalButton>
                 </Modal.Footer>
             </Modal>
         </div>
--- src/pages/InterviewFeedback.js
+++ src/pages/InterviewFeedback.js
@@ -1,95 +1,104 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHeaderMode } from '../hooks/HeaderManager';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Box from '../components/Box';
 import '../styles/login.css';
 import Button from '../components/Button';
 import Font from '../components/Font';
-import styled from 'styled-components';
-import Input from "../components/Input";
 import { FaStar } from 'react-icons/fa'; // 별점 아이콘
-
 import ReviewForm from '../components/ReviewForm ';
-
+import axios from "axios";
 
 const InterviewFeedback = () => {
+    let accessToken = window.localStorage.getItem('accessToken');
     const { headerMode, setHeaderMode } = useHeaderMode();
     const navigate = useNavigate();
     const [userScore, setUserScore] = useState(0); // 사용자의 별점
     const [userReview, setUserReview] = useState(''); // 사용자의 리뷰
     const [submittedReviews, setSubmittedReviews] = useState([]); // 제출된 리뷰들
+    let roomId = 0; // 실제 값 할당 필요
+    let mentorId = 0; // 실제 값 할당 필요
+    const [feedback, setFeedback] = useState('');
+    const [mentorNickname, setMentorNickname] = useState('');
+    const [content, setContent] = useState('');
 
-    const handleSubmitReview = (newReview) => {
-        setSubmittedReviews((prevReviews) => [...prevReviews, newReview]); // 리뷰 추가
-    };
 
     useEffect(() => {
         setHeaderMode('main');
     }, [setHeaderMode]);
 
+    const handleSubmitReview = async () => {
+        const newReview = {
+            score: userScore,
+            userComment: userReview,
+        };
+
+        setSubmittedReviews((prevReviews) => [...prevReviews, newReview]); // 리뷰 추가
+
+        // 피드백 API 호출
+        await handleMentorFeedback();
+    };
+
+    const handleGetMenteeFeedback = async () => {
+        try {
+            const response = await  axios.get(process.env["REACT_APP_API_URL "] + roomId,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+            const data = response.data.data;
+            setContent(data.content);
+            setMentorNickname(data.mentorNickname);
+        } catch (error) {
+            alert("멘티 피드백을 불러오는데 실패했습니다.")
+        }
+    }
+
+    useEffect(() => {
+        handleGetMenteeFeedback();
+    }, [accessToken])
+
+    const handleMentorFeedback = async () => {
+        try {
+            const response = await axios.post(`http://localhost:8080/mentee/feedback/${roomId}/${mentorId}`,
+                {
+                    content: feedback,
+                    starRating: userScore,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                }
+            );
+            alert("멘토 피드백이 제출되었습니다!");
+        } catch (error) {
+            alert("멘토 피드백 작성에 실패했습니다. 다시 시도해 주세요");
+        }
+    };
 
     return (
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start' }}>
-            <Box
-                height="100%"
-                width="35vw"
-                border="none"
-                alignItems="flex-start"
-                justify="flex-start"
-                style={{ display: 'flex' }} // 자식 박스에서 정렬
-            >
+            <Box height="100%" width="35vw" border="none" alignItems="flex-start" justify="flex-start" style={{ display: 'flex' }}>
                 <div style={{ marginBottom: '5px', width: '100%' }}>
-                    <Font
-                        font="PretendardL"
-                        size="22px"
-                        color="#000000"
-                        margintop="5px"
-                        spacing="2px"
-                        paddingleft="13px"
-                        paddingtop="5px"
-                        marginbottom="8px"
-                    >
-                        멘토 피드백
+                    <Font font="PretendardL" size="22px" color="#000000" margintop="5px" spacing="2px" paddingleft="13px" paddingtop="5px" marginbottom="8px">
+                        {mentorNickname} 피드백
                     </Font>
                 </div>
-                <Box
-                    height="57vh"
-                    width="30vw"
-                    border="none"
-                    alignItems="center"
-                    justify="center"
-                    top="10px"
-                    bottom="10px"
-                    style={{ display: 'flex' }} // 자식 박스에서 정렬
-                >
+                <Box height="57vh" width="30vw" border="none" alignItems="center" justify="center" top="10px" bottom="10px" style={{ display: 'flex' }}>
+                    <Font font="PretendardB" size="18px" color="#000000" margintop="5px" spacing="2px" paddingleft="13px" paddingtop="5px" marginbottom="8px">
+                        멘토링 후기를 작성해주세요!
+                    </Font>
                 </Box>
-                <Box
-                    height="28vh"
-                    width="30vw"
-                    border="none"
-                    alignItems="flex-start"
-                    justify="flex-start"
-                    top="10px"
-                    direction='column'
-                    bottom="10px"
-                    style={{ display: 'flex', flexDirection: 'column' }} // 자식 박스에서 정렬
-                >
+                <Box height="28vh" width="30vw" border="none" alignItems="flex-start" justify="flex-start" top="10px" direction='column' bottom="10px" style={{ display: 'flex', flexDirection: 'column' }}>
                     <div style={{ marginBottom: '0px', width: '100%' }}>
-                        {/* 텍스트와 별점을 수평으로 정렬 */}
                         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <Font
-                                font="PretendardB"
-                                size="18px"
-                                color="#000000"
-                                margintop="5px"
-                                spacing="2px"
-                                paddingleft="13px"
-                                paddingtop="5px"
-                                marginbottom="8px"
-                            >
+                            <Font font="PretendardB" size="18px" color="#000000" margintop="5px" spacing="2px" paddingleft="13px" paddingtop="5px" marginbottom="8px">
                                 멘토링 후기를 작성해주세요!
                             </Font>
-                            {/* 별점 컴포넌트가 텍스트 오른쪽에 수평으로 위치 */}
                             <div style={{ display: 'flex', marginLeft: '8px' }}>
                                 {Array.from({ length: 5 }, (_, index) => (
                                     <FaStar
@@ -104,8 +113,7 @@ const InterviewFeedback = () => {
                         </div>
                     </div>
 
-                    <ReviewForm onSubmitReview={handleSubmitReview} /> {/* ReviewForm 컴포넌트 사용 */}
-                    {/* 제출된 리뷰 목록 */}
+                    <ReviewForm onSubmitReview={handleSubmitReview} setUserReview={setUserReview} /> {/* ReviewForm 컴포넌트 사용 */}
                     <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
                         <div style={{ height: "20px", width: "30px" }}></div>
                         <Button
@@ -121,15 +129,7 @@ const InterviewFeedback = () => {
                     </div>
 
                     <div>
-                        {submittedReviews.map((review, index) => (
-                            <div key={index}>
-                                <div>익명</div> {/* 익명 표시 */}
-                                <div>{review.score}점</div>
-                                <div>{review.userComment}</div>
-                            </div>
-                        ))}
                     </div>
-
                 </Box>
             </Box>
         </div>
